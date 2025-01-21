7969815121f692037f86a1f88ec117b7b039d83d
lilingfengdev <lilingfengvvv@gmail.com>
2025-01-21 18:14:29 +0800

    add raytraceantixray support

--- src/auto_antixray.py
+++ src/auto_antixray.py
@@ -1,3 +1,5 @@
+import os.path
+
 from utils import *
 
 script_license()
@@ -78,9 +80,95 @@ def config_paper_end(paper):
     antixray_config(paper)
     paper["anticheat"]["anti-xray"]["enabled"] = False
 
-
+def config_raytrace():
+    with open("plugins/RayTraceAntiXray/config.yml","w",encoding="utf-8") as handler:
+        if ask("使用 RayTraceAntiXray 优化配置(选NO使用安全配置)"):
+            handler.write("""
+settings:
+  anti-xray:
+    update-ticks: 1
+    ms-per-ray-trace-tick: 50
+    # 根据可用的（最好是未使用的）CPU线程进行调整。
+    ray-trace-threads: 2
+world-settings:
+  default:
+    anti-xray:
+      ray-trace: true
+      ray-trace-third-person: false
+      ray-trace-distance: 64.0
+      rehide-blocks: false
+      rehide-distance: .inf
+      max-ray-trace-block-count-per-chunk: 30
+      ray-trace-blocks:
+      # 你可以在这里添加更多的方块，
+      # 但可能需要调整max-ray-trace-block-count-per-chunk设置。
+      - chest
+      - diamond_ore
+      - deepslate_diamond_ore
+      - emerald_ore
+      - deepslate_emerald_ore
+      - gold_ore
+      - deepslate_gold_ore
+      - lapis_ore
+      - deepslate_lapis_ore
+      - spawner
+  world_nether:
+    anti-xray:
+      # 注意，ancient_debris(下界合金)永远不会自然生成在暴露于空气的地方。
+      # 普通引擎模式：1已经足够，在下界禁用射线追踪。
+      ray-trace: false
+  # 调整世界名称。
+  world_the_end:
+    anti-xray:
+      ray-trace: false
+            """)
+        else:
+            handler.write("""
+settings:
+  anti-xray:
+    update-ticks: 1
+    ms-per-ray-trace-tick: 50
+    # 根据可用的（最好是未使用的）CPU线程进行调整。
+    ray-trace-threads: 2
+world-settings:
+  default:
+    anti-xray:
+      ray-trace: true
+      # 请注意，这大约需要三倍的资源。
+      ray-trace-third-person: true
+      ray-trace-distance: 80.0
+      rehide-blocks: true
+      rehide-distance: 76.0
+      max-ray-trace-block-count-per-chunk: 60
+      ray-trace-blocks:
+      # 您可以在此处添加更多方块，
+      # 但可能需要调整max-ray-trace-block-count-per-chunk设置。
+      - chest
+      - diamond_ore
+      - deepslate_diamond_ore
+      - emerald_ore
+      - deepslate_emerald_ore
+      - gold_ore
+      - deepslate_gold_ore
+      - lapis_ore
+      - deepslate_lapis_ore
+      - mossy_cobblestone
+      - spawner
+  # 调整世界名称。
+  world_nether:
+    anti-xray:
+      # 注意，ancient_debris(下界合金)永远不会自然生成在暴露于空气的地方。
+      # 普通引擎模式：1已经足够，在下界禁用射线追踪。
+      ray-trace: false
+  # 调整世界名称。
+  world_the_end:
+    anti-xray:
+      ray-trace: false
+            """)
 if __name__ == "__main__":
     config_paper_world()
     config_paper_nether()
     config_paper_end()
+    if os.path.exists("plugins/RayTraceAntiXray/config.yml"):
+        config_raytrace()
     exit_()
