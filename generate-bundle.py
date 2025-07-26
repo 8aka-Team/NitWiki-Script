456aec2b9a6e1bcbcce1e91d36687ae53c286d6e
lilingfengdev <145678359+lilingfengdev@users.noreply.github.com>
2025-07-26 09:02:19 +0800

    Update generate-bundle.py

--- generate-bundle.py
+++ generate-bundle.py
@@ -4,6 +4,11 @@ import subprocess
 import platform
 import tempfile
 from concurrent.futures import ThreadPoolExecutor
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 os.system("python3 -m pip install pyyaml tqdm psutil requests imageio rtoml elevate colorama nuitka ordered-set")
 
